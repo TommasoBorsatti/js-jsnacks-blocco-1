11838a48a9493caa4feb59ffcad40cdfeae17be3
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-03 13:12:37 +0100

    Snack 8

---
+++ Snack 8/js/script.js
@@ -0,0 +1,15 @@
+
+
+// Chiedi un numero di 4 cifre all’utente e calcola la somma
+//di tutte le cifre che compongono il numero.
+
+var somma = 0;
+var numero = (prompt("Inserire un numero di 4 cifre"));
+
+for (var i = 0; i < numero.length ; i++ ){
+
+  somma += parseInt(numero[i]);
+
+}
+
+ console.log(somma);
