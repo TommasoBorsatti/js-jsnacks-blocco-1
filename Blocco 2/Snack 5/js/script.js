d81931dea4b8887062165f4d9888828a858a170c
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-05 10:00:12 +0100

    Correzione Snack Blocco 2

--- Blocco 2/Snack 5/js/script.js
+++ Blocco 2/Snack 5/js/script.js
@@ -4,19 +4,42 @@ Crea due array che hanno un numero di elememnti diversi.
 Aggiungi elementi casuali all'array che ha meno elementi, fino a quando non ne avrà quanti l'altro.
 */
 
-alert("Questo programma ti aiuta a fare la lista della spesa! Scrivi qui di seguito i nomi di alcuni prodotti da comprare: continuerò a chiederteli fino a quando non avrai scelto un numero di prodotti uguale a quello della lista che ho scritto io.");
-
-
+// alert("Questo programma ti aiuta a fare la lista della spesa! Scrivi qui di seguito i nomi di alcuni prodotti da comprare: continuerò a chiederteli fino a quando non avrai scelto un numero di prodotti uguale a quello della lista che ho scritto io.");
+//
+//
 var lista1 = [];
 var lista2 = [ "Insalata", "Cavoli Rossi", "Zucca", "Formaggio", "Vino", "Finocchi"];
 var prodotto = 0;
+//
+//
+//
+//
+
+// ESERCIZIO MIGLIORATO
+
+
+// determino array piu lungo
+
+if (lista1.length > lista2.length) {
+
+  var maxArray = lista1 ;
+  var minArray = lista2 ;
+
+} else if (lista2.length >lista1.length) {
+
+  var maxArray = lista2 ;
+  var minArray = lista1 ;
+
+} else {
+  alert("i panieri sono lunghi uguali")
+}
 
 do {
 
   prodotto = prompt("Aggiungi altri elementi alla lista della spesa, scrivendoli qui:");
-  lista1.push(prodotto);
+  minArray.push(prodotto);
 
-} while (lista2.length > lista1.length);
+} while (maxArray.length > minArray.length);
 
 console.log("Questi sono gli oggetti che hai scelto tu: " + lista1 + " la tua lista contiene esattamente: " + lista1.length + " prodotti!");
 console.log(lista1);
